6eb8165bcc84b24b706a54f2bf729c66d8dec407
Sashikumar Yadav <yshashi30@gmail.com>
2025-09-10 19:22:49 +0400

    Feature/create-ui-for-events (#21)

---
+++ apps/frontend/src/app/shared/types/event.ts
@@ -0,0 +1,14 @@
+export interface Event {
+  id: string;
+  conference: string;
+  eventType: string;
+  date: string;
+  location: string;
+  name: string;
+  sport: string;
+  description: string;
+  time: string;
+  capacity: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
--- apps/frontend/src/app/shared/types/index.ts
+++ apps/frontend/src/app/shared/types/index.ts
@@ -1,2 +1,3 @@
 export { AuthResponse } from './auth-response';
 export { UserProfile } from './user-profile';
+export { Event } from './event';
