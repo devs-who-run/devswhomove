687d06d223172028ef20aaa0fb89ed1282bd877e
Sashikumar <yshashi30@gmail.com>
2026-02-11 21:21:36 +0200

    refactor: remove unused create-event library and update app routes

--- apps/frontend/libs/event/create/src/lib/lib.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Route } from '@angular/router';
-import { CreateEventComponent } from './create-event/create-event.component';
-
-export const createEventRoutes: Route[] = [
-  { path: 'event/create', component: CreateEventComponent },
-];
